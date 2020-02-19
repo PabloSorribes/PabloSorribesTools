838d8450aa7260485811f05817a9401d107f6a2c
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2020-02-19 23:12:02 +0100

    Implementing a DragAndDrop window which works for most things. Gonna do this into a Helper-class instead, like the EditorGUIHelper

--- Assets/WIP Packages/Editor/DragAndDropFiles/Editor/DragAndDropFilesWindow.cs
+++ Assets/WIP Packages/Editor/DragAndDropFiles/Editor/DragAndDropFilesWindow.cs
@@ -1,19 +1,17 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace Paalo.Tools
 {
-
-
 	public class DragAndDropFilesWindow : EditorWindow
 	{
+		#region ToolName and SetupWindow
 		private const int menuIndexPosition = PabloSorribesToolsConstants.defaultPaaloMenuIndexPosition;     //To make the menu be at the top of the GameObject-menu and the first option in the hierarchy.
 		private const string baseMenuPath = PabloSorribesToolsConstants.defaultPaaloMenuPath;
 		private const string rightClickMenuPath = "GameObject/" + baseMenuPath + toolName;
 		private const string toolsMenuPath = "Window/" + baseMenuPath + toolName;
 		private const string toolName = "Drag and Drop AudioClips Window";
-
-		public AudioClip[] audioClips = null;
 
 		[MenuItem(rightClickMenuPath, false, menuIndexPosition)]
 		public static void RightClickMenu()
@@ -33,30 +31,58 @@ namespace Paalo.Tools
 			window.minSize = new Vector2(300, 200);
 			window.maxSize = new Vector2(window.minSize.x + 100, window.minSize.y + 100);
 		}
+		#endregion ToolName and SetupWindow
+
+		#region Tool-specific Variables
+		public AudioClip[] audioClips = null;
+		/// <summary>
+		/// Use this action to get an array of the objects that were dragged into the area.
+		/// </summary>
+		public static event System.Action<UnityEngine.Object[]> OnDragPerformed;
+		#endregion
+
+		private void OnEnable()
+		{
+			OnDragPerformed += UpdateClipsOnDragPerform;
+		}
+
+		private void UpdateClipsOnDragPerform<T>(T[] draggedObjects) where T : Object
+		{
+			Debug.Log($"Dragged Obj Array: {draggedObjects.GetType().FullName}");
+
+			foreach (var draggedObj in draggedObjects)
+			{
+				Debug.Log($"Dragged Obj: {draggedObj.GetType().FullName}");
+			}
+
+			audioClips = draggedObjects as AudioClip[];
+			Debug.Log("Array length: " + audioClips.Length);
+		}
 
 		private void OnGUI()
 		{
 			EditorGUILayout.Space();
-			GUISection_DragAndDropArea();
+			DrawDragAndDropArea<AudioClip>(new DragAndDropAreaInfo());
 			EditorGUILayout.Space();
 		}
 
-		private void GUISection_DragAndDropArea()
+		/// <summary>
+		/// Draws a Drag and Drop Area and raises the <see cref="OnDragPerformed"/>-event, which you can use use to get an array of the objects that were dragged into the area.
+		/// </summary>
+		/// <typeparam name="T">The object type you want the <see cref="OnDragPerformed"/> method to handle.</typeparam>
+		/// <param name="dragAreaInfo"></param>
+		/// <returns></returns>
+		public static void DrawDragAndDropArea<T>(DragAndDropAreaInfo dragAreaInfo) where T : UnityEngine.Object
 		{
+			//Change color and create Drag Area
 			Color originalGUIColor = GUI.color;
-
-			GUI.color = Color.black;
+			GUI.color = dragAreaInfo.outlineColor;
 			EditorGUILayout.BeginVertical(GUI.skin.box);
+			GUI.color = dragAreaInfo.backgroundColor;
+			var dragArea = GUILayoutUtility.GetRect(dragAreaInfo.dragAreaWidth, dragAreaInfo.dragAreaHeight, GUILayout.ExpandWidth(true));
+			GUI.Box(dragArea, dragAreaInfo.DragAreaText);
 
-			//GUI.color = DTGUIHelper.BrightTextColor;
-			GUI.color = Color.yellow;
-
-			var dragArea = GUILayoutUtility.GetRect(0f, 35f, GUILayout.ExpandWidth(true));
-			//GUI.Box(dragArea, MasterAudio.DragAudioTip + " to create Variations!");
-			GUI.Box(dragArea, "Drag Audio clips or a folder containing some Audio Clips here!");
-
-			GUI.color = Color.white;
-
+			//See if the current Editor Event is a DragAndDrop event.
 			var anEvent = Event.current;
 			switch (anEvent.type)
 			{
@@ -64,55 +90,104 @@ namespace Paalo.Tools
 				case EventType.DragPerform:
 					if (!dragArea.Contains(anEvent.mousePosition))
 					{
+						//Early Out in case the drop is made outside the drag area.
 						break;
 					}
 
+					//Change mouse cursor icon to the "Copy" icon
 					DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
 
+					//If mouse is released 
 					if (anEvent.type == EventType.DragPerform)
 					{
 						DragAndDrop.AcceptDrag();
-
-						foreach (var dragged in DragAndDrop.objectReferences)
-						{
-							if (dragged is DefaultAsset)
-							{
-								var assetPaths = AssetDatabase.FindAssets("t:AudioClip", DragAndDrop.paths);
-								foreach (var assetPath in assetPaths)
-								{
-									var clip = AssetDatabase.LoadAssetAtPath<AudioClip>(AssetDatabase.GUIDToAssetPath(assetPath));
-									if (clip == null)
-									{
-										continue;
-									}
-
-									//Add clip to list of clips to set on AudioSources
-
-									Debug.Log(clip.name);
-
-									//CreateVariation(_group, _ma, clip);
-								}
-
-								continue;
-							}
-
-							var aClip = dragged as AudioClip;
-							if (aClip == null)
-							{
-								continue;
-							}
-
-							Debug.Log(aClip.name);
-
-
-							//CreateVariation(_group, _ma, aClip);
-						}
+						var draggedTypeObjectsArray = GetDraggedObjects<T>();
+						OnDragPerformed?.Invoke(draggedTypeObjectsArray);
 					}
+
 					Event.current.Use();
 					break;
 			}
+
 			EditorGUILayout.EndVertical();
 			GUI.color = originalGUIColor;
 		}
+
+		private static T[] GetDraggedObjects<T>() where T : Object
+		{
+			List<T> draggedTypeObjects = new List<T>();
+
+			foreach (var dragged in DragAndDrop.objectReferences)
+			{
+				T draggedAsset = null;
+
+				//A "DefaultAsset" is a folder in the Unity Editor.
+				if (dragged is DefaultAsset)
+				{
+					var assetPaths = AssetDatabase.FindAssets("t:AudioClip", DragAndDrop.paths);
+					foreach (var assetPath in assetPaths)
+					{
+						draggedAsset = AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(assetPath));
+						if (draggedAsset == null)
+						{
+							continue;
+						}
+
+						Debug.Log($"Default Asset Dragged: {draggedAsset.name}");
+						draggedTypeObjects.Add(draggedAsset as T);
+					}
+					continue;
+				}
+
+				draggedAsset = dragged as T;
+				if (draggedAsset == null)
+				{
+					continue;
+				}
+
+				//Debug.Log($"Asset of type '{draggedAsset.GetType().FullName}' dragged. Asset Name: '{draggedAsset.name}'");
+				draggedTypeObjects.Add(draggedAsset as T);
+			}
+			return draggedTypeObjects.ToArray();
+		}
 	}
+
+	/// <summary>
+	/// Use this class to create some values for the DragAndDrop-area that you want to create.
+	/// </summary>
+	public class DragAndDropAreaInfo
+	{
+		public string DragAreaText
+		{
+			get => $"Drag {draggedObjectTypeName} or a folder containing some {draggedObjectTypeName} here!";
+			//private set => DragAreaText = value;
+		}
+
+		public string draggedObjectTypeName = "AudioClips";
+		public float dragAreaWidth = 0f;
+		public float dragAreaHeight = 35f;
+
+		public Color outlineColor = Color.black;
+		public Color backgroundColor = Color.yellow;
+
+		//Creates a default class with the values set in the definition above.
+		public DragAndDropAreaInfo()
+		{
+		}
+
+		public DragAndDropAreaInfo(string draggedObjectTypeName)
+		{
+			this.draggedObjectTypeName = draggedObjectTypeName;
+		}
+
+		public DragAndDropAreaInfo(string draggedObjectTypeName, Color outlineColor, Color backgroundColor, float dragAreaWidth = 0f, float dragAreaHeight = 35f)
+		{
+			this.draggedObjectTypeName = draggedObjectTypeName;
+			this.outlineColor = outlineColor;
+			this.backgroundColor = backgroundColor;
+			this.dragAreaWidth = dragAreaWidth;
+			this.dragAreaHeight = dragAreaHeight;
+		}
+	}
+
 }
