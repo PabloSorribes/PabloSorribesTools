34ce442f86879f95c47fb4a1c7d37d97f9d23f27
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2020-05-24 04:34:30 +0200

    Adding a CurveBehaviour component to try out AnimationCurves for mapping velocity (m/s) to volume (dB)

---
+++ Assets/WIP Packages/Runtime/CurveBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Paalo
+{
+	public class CurveBehaviour : MonoBehaviour
+	{
+		public float speed = 2f;
+
+		//Default values for: minSpeedThreshold, volumeRangeLow, maxSpeedClamp, volumeRangeHigh
+		public AnimationCurve animationCurve = AnimationCurve.EaseInOut(2f, -12f, 10f, 0f);
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Alpha1))
+			{
+				//Get the different keys
+				Keyframe firstKey = animationCurve.keys[0];
+				Keyframe lastKey = animationCurve.keys[animationCurve.length - 1];
+
+				// Min / Max speed
+				float minSpeedThreshold = firstKey.time;
+				float maxSpeedClamp = lastKey.time;
+
+				// Volume Range
+				float volumeRangeLow = firstKey.value;
+				float volumeRangeHigh = lastKey.value;
+
+				//Calculate volume reduction
+				float volumeReductionDB = animationCurve.Evaluate(speed);
+				Debug.Log($"VolumeReductionDB: {volumeReductionDB}");
+			}
+
+			if (Input.GetKeyDown(KeyCode.Alpha2))
+			{
+				Debug.Log($"Move key!");
+				animationCurve.MoveKey(animationCurve.length - 1, new Keyframe(10f, -6f));
+			}
+		}
+	}
+}
