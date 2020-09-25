d63a56d1880711b782d9e3c26859c063c8783a41
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2020-09-25 11:25:24 +0200

    changed variables in AnimationCurveBehaviour to be more clear on what they do. Eg. "speed" → "velocity" and "animationCurve" → "velocityToDecibelAnimCurve"

---
+++ Assets/WIP Packages/Runtime/AnimationCurveBehaviour/AnimationCurveBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Paalo.WIP
+{
+	public class AnimationCurveBehaviour : MonoBehaviour
+	{
+		public float velocity = 2f;
+
+		//Default values for: minVelocityThreshold, volumeRangeLow, maxVelocityClamp, volumeRangeHigh
+		public AnimationCurve velocityToDecibelAnimCurve = AnimationCurve.EaseInOut(2f, -12f, 10f, 0f);
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Alpha1))
+			{
+				// Get the different keys
+				Keyframe firstKey = velocityToDecibelAnimCurve.keys[0];
+				Keyframe lastKey = velocityToDecibelAnimCurve.keys[velocityToDecibelAnimCurve.length - 1];
+
+				// Min / Max speed
+				float minVelocityThreshold = firstKey.time;
+				float maxVelocityClamp = lastKey.time;
+
+				// Volume Range
+				float volumeRangeLow = firstKey.value;
+				float volumeRangeHigh = lastKey.value;
+
+				// Calculate volume reduction
+				float volumeReductionDB = velocityToDecibelAnimCurve.Evaluate(velocity);
+				Debug.Log($"Velocity:\t{velocity}\n" +
+						$"VolumeReductionDB:\t{volumeReductionDB} dB");
+			}
+
+			// How to change the value of a key in runtime
+			if (Input.GetKeyDown(KeyCode.Alpha2))
+			{
+				velocityToDecibelAnimCurve.MoveKey(velocityToDecibelAnimCurve.length - 1, new Keyframe(10f, -6f));
+				Debug.Log($"Set 'VolumeRangeHigh' to -6 dB");
+			}
+		}
+	}
+}
